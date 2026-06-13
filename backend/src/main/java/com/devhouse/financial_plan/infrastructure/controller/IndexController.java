65a6413b819df2bcfc1744ad87d252a7ee2a1fe4
otaviomoura <otavio.moura120@gmail.com>
2026-06-13 13:10:42 -0300

    config auth0

--- backend/src/main/java/com/devhouse/financial_plan/infrastructure/controller/IndexController.java
+++ backend/src/main/java/com/devhouse/financial_plan/infrastructure/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.devhouse.financial_plan.infrastructure.controller;
 
+import com.auth0.spring.boot.Auth0AuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello World!";
+    public String index(Authentication authentication) {
+        Auth0AuthenticationToken auth0Token = (Auth0AuthenticationToken) authentication;
+
+        return "Hello World!" + auth0Token.getName();
     }
 }
